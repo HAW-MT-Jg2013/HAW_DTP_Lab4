7007f22b511bdda029eff617cb67c4cd364c1354
Jannik Beyerstedt <jtByt.Pictures@gmail.com>
2014-12-22 21:00:37 +0100

    [TOOL] change background

---
+++ DTP_Lab4_X_modelsim/WaveBackgroundBlack.tcl
@@ -0,0 +1,27 @@
+# Diese TCL-Datei setzt im ModelSim Wave-Fenster den Hintergrund auf Schwarz,
+# um augenschonender arbeiten zu können.
+# Um die Datei anzuwenden, ist sie ins ModelSim-Projekt einzubinden. Durch Klick mit rechter Maustaste
+# und Auswahl "Execute" wird die Datei ausgeführt.
+# Alternativ kann sie auch über Tools-> TCL -> Execute Macro ausgeführt werden.
+#
+set PrefWave(waveBackground) Black
+set PrefWave(arcFillColor) #1c95bd
+set PrefWave(geometry) 544x250+472+259
+set PrefWave(gridColor) Gray70
+set PrefWave(boxFillColor) #2255f0
+set PrefWave(font) {{Courier New} 10 roman bold}
+set PrefWave(background) Black
+set PrefWave(vectorColor) ForestGreen  
+set PrefWave(foreground) White
+set PrefWave(textColor) White
+set PrefWave(cursorColor) SteelBlue
+set PrefWave(timeColor) White
+set PrefWave(cursorDeltaColor) SteelBlue
+set PrefDataflow(geometry) 228x139+472+555
+set LogicStyleTable(LOGIC_DC) {DoubleDash brown 1}
+set LogicStyleTable(LOGIC_W) {DoubleDash red 1}
+set LogicStyleTable(LOGIC_0) {Solid green 0}
+set LogicStyleTable(LOGIC_X) {Solid red 1}
+set LogicStyleTable(LOGIC_1) {Solid green 2}
+set LogicStyleTable(LOGIC_U) {Solid red 1}
+set LogicStyleTable(LOGIC_Z) {Solid Blue 1}
---
+++ DTP_Lab4_X_modelsim/WaveBackgroundWhite.tcl
@@ -0,0 +1,29 @@
+# Diese TCL-Datei setzt im ModelSim Wave-Fenster den Hintergrund auf Weiss,
+# um bei einem Sreenshot und Einbinden in ein Textdokument beim Ausdruck Tinte zu sparen.
+# Um die Datei anzuwenden, ist sie ins ModelSim-Projekt einzubinden. Durch Klick mit rechter Maustaste
+# und Auswahl "Execute" wird die Datei ausgeführt.
+# Alternativ kann sie auch über Tools-> TCL -> Execute Macro ausgeführt werden.
+#
+set PrefWave(waveBackground) White
+set PrefWave(arcFillColor) #1c95bd
+set PrefWave(geometry) 544x250+472+259
+set PrefWave(gridColor) Gray70
+set PrefWave(boxFillColor) #2255f0
+set PrefWave(font) {{Courier New} 10 roman bold}
+set PrefWave(background) White
+set PrefWave(selectbackground) Gray70
+set PrefWave(vectorColor) Black
+set PrefWave(foreground) Black
+set PrefWave(textColor) Black
+set PrefWave(cursorColor) Magenta
+set PrefWave(timeColor) Black
+set PrefWave(cursorDeltaColor) Magenta
+set PrefDataflow(geometry) 228x139+472+555
+set LogicStyleTable(LOGIC_DC) {DoubleDash blue 1}
+set LogicStyleTable(LOGIC_W) {DoubleDash red 1}
+set LogicStyleTable(LOGIC_0) {Solid Blue 0}
+set LogicStyleTable(LOGIC_X) {Solid red 1}
+set LogicStyleTable(LOGIC_1) {Solid Blue 2}
+set LogicStyleTable(LOGIC_U) {Solid red 1}
+set LogicStyleTable(LOGIC_Z) {Solid Green 1}
+
